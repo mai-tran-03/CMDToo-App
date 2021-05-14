ed5544130f25c80edb2ba4248291045ac88eef46
Maya Murphy <murphym2@carleton.edu>
2021-05-14 10:54:48 -0500

    updated homescreen

--- App.js
+++ App.js
@@ -1,30 +1,34 @@
 import { StatusBar } from "expo-status-bar";
-import React, { Fragment } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import CustomButton from "./components/CustomButton.js";
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+import CustomButtom from "./components/CustomButton.js";
+import styled from "styled-components/native";
+
+const StyledView = styled.View`
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+`;
+
+const AppName = styled.Text`
+  font-size: 35px;
+`;
+
+const AppDesc = styled.Text`
+  padding-vertical: 15px;
+  font-size: 18px;
+`;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Fragment>
-        <Text style={{ fontSize: 35 }}>GEN CONNECT</Text>
-        <Text style={{ paddingVertical: 15, fontSize: 18 }}>
-          Invite your child to share their feelings
-        </Text>
-        <CustomButton text="play" color="#9BC53D" />
-        <CustomButton text="parent guide" color="#46C1C1" />
-        <CustomButton text="how to play" color="#EE3282" />
-      </Fragment>
+    <StyledView>
+      <AppName> GEN CONNECT </AppName>
+      <AppDesc> Invite your child to share their feelings </AppDesc>
+      <CustomButtom text="play" color="#9BC53D" />
+      <CustomButtom text="parent guide" color="#46C1C1" />
+      <CustomButtom text="how to play" color="#EE3282" />
       <StatusBar style="auto" />
-    </View>
+    </StyledView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
