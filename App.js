e7a9fe70e4c81f108a152a24165e862d47fca352
Etienne Richart <etiennerichart@gmail.com>
2021-05-15 22:59:23 -0500

    Add navigation and change styling

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import CustomButtom from "./components/CustomButton.js";
+import CustomButton from "./components/CustomButton.js";
+import CustomText from './components/CustomText.js';
 import styled from "styled-components/native";
 import { ThemeProvider } from 'styled-components';
 import COLORS from "./components/GlobalStyles.js";
@@ -21,17 +25,56 @@ const AppDesc = styled.Text`
   font-size: 18px;
 `;
 
+
+function HomeScreen({ navigation }) {
+  return (
+    <StyledView>
+        <AppName> GEN CONNECT </AppName>
+        <AppDesc> Invite your child to share their feelings </AppDesc>
+        <CustomButton text="play" color="dance challenge" onPress={() => navigation.navigate('Play')} />
+        <CustomButton text="parent guide" color="all about me" />
+        <CustomButton text="how to play" color="the inner me" onPress={() => navigation.navigate('HowToPlay')} />
+        <StatusBar style="auto" />
+    </StyledView>
+  );
+}
+
+function HowToPlay({ navigation }) {
+  return (
+    <StyledView>
+        <CustomText text='Press "PLAY"' color="dance challenge" count="1"/>
+        <CustomText text="Pick an order of players" color="the inner me" count="2"/>
+        <CustomText text="Pick a Category" color="what would you do" count="3"/>
+        <CustomText text="Answer the Question" color="all about me" count="4" />
+        <CustomText text="Pass the phone to the next person" color="my bright future"count="5" />
+        <CustomButton text="play" color="dance challenge" onPress={() => navigation.navigate('Play')}/>
+        <StatusBar style="auto" />
+    </StyledView>
+  );
+}
+
+function Play({ navigation }) {
+  return (
+    <StyledView>
+        <CustomButton text="home" color="dance challenge" onPress={() => navigation.navigate('Home')} />
+        <StatusBar style="auto" />
+    </StyledView>
+  );
+}
+
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
+    <NavigationContainer>
     <ThemeProvider theme={{ colors: COLORS }} >
-      <StyledView>
-      <AppName> GEN CONNECT </AppName>
-      <AppDesc> Invite your child to share their feelings </AppDesc>
-      <CustomButtom text="play" color="dance challenge" />
-      <CustomButtom text="parent guide" color="all about me" />
-      <CustomButtom text="how to play" color="the inner me" />
-    <StatusBar style="auto" />
-    </StyledView>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Play" component={Play} />
+        <Stack.Screen name="HowToPlay" component={HowToPlay} />
+      </Stack.Navigator>
     </ThemeProvider>
+    </NavigationContainer>
+    
   );
 }
--- components/CustomText.js
+++ components/CustomText.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components/native";
 
-const ButtonContainer = styled.TouchableOpacity`
+const ButtonContainer = styled.View`
   align-self: stretch;
   background-color: ${(props) => props.theme.colors[props.color]};
   border-radius: 20px;
@@ -16,13 +16,22 @@ const ButtonText = styled.Text`
   color: white;
   text-align: center;
   font-size: 25px;
+  margin: 0 30px;
   /* padding-bottom: 11px; */
 `;
 
-const CustomButton = ({ text, color, ...others }) => (
-  <ButtonContainer color={color} onPress={others.onPress}>
+const Count = styled.Text`
+  position: absolute;
+  top: 6px;
+  left: 6px;
+  color: ${(props) => props.theme.colors.white}
+`;
+
+const CustomText = ({ text, color, count }) => (
+  <ButtonContainer color={color}>
+    <Count>{count}.</Count>
     <ButtonText>{text}</ButtonText>
   </ButtonContainer>
 );
 
-export default CustomButton;
+export default CustomText;
