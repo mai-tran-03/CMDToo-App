3dd765f396ebbfb06d363377c84e7b4ba6294eb7
Etienne Richart <etiennerichart@gmail.com>
2021-06-29 03:04:11 -0500

    added search

--- components/ParentGuide.js
+++ components/ParentGuide.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import qs from './questions.json';
 import CustomButton from './CustomButton.js';
-import { TextInput } from 'react-native';
 import styled from 'styled-components/native';
 import {
-    StyledView,
     ScrollStyledView,
     ParentGuideContainer,
     ViewHeading,
     ViewBy
 } from './StyledView';
+import { View } from 'react-native';
 
 const SearchBar = styled.TextInput`
     border-radius: 5px;
@@ -20,12 +20,100 @@ const SearchBar = styled.TextInput`
     align-self: stretch;
     margin: 0 20px;
     padding: 5px 30px 5px 10px;
+    z-index: 2;
+`;
+
+export const StyledView = styled.View``;
+
+const AutoMatch = styled.Text`
+    padding-left: 15px;
+    margin: 7.5px 0;
+    font-size: 20px;
+    font-size: 13px;
+    font-family: Avenir;
+    line-height: 18px;
+    align-content: center;
+`;
+
+const MatchBorder = styled.View`
+    border-top-color: black;
+    border-top-width: 1px;
+`;
+
+const StyledMargin = styled.View`
+    width: 100%;
+    position: absolute;
+    top: 27px;
+    left: 0;
+    z-index: 1;
+`;
+
+const StyledList = styled.FlatList`
+    background-color: white;
+    background-color: white;
+    margin: 0 23px;
+    border-bottom-right-radius: 5px;
+    border-bottom-left-radius: 5px;
 `;
 
 export function ParentGuide({ navigation }) {
+    const [input, setInput] = useState('');
+    const [output, setOutput] = useState('');
+    useEffect(() => {
+        setOutput(find(input));
+    }, [input]);
+
+    const [past, setPast] = useState({});
+
+    const find = str_to_match => {
+        if (!str_to_match) return [];
+        let prev = str_to_match.slice(0, -1).toLowerCase();
+        let options;
+
+        if (prev.length > 0 && prev in past) {
+            options = qs.filter(q => past[prev].includes(q.Question));
+        } else {
+            options = qs;
+        }
+        let reg_ex = new RegExp(str_to_match, 'i');
+        let matches = new Set(
+            options.filter(
+                o =>
+                    o.Question.match(reg_ex) ||
+                    o.Category.match(reg_ex) ||
+                    o.Group.match(reg_ex) ||
+                    (o['Follow Up'].length > 0 &&
+                        o['Follow Up'].reduce(
+                            (bool, q) => q.match(reg_ex) || bool,
+                            false
+                        ))
+            )
+        );
+        setPast(prevState => ({
+            ...prevState,
+            [str_to_match.toLowerCase()]: [...matches].map(m => m.Question)
+        }));
+        return [...matches];
+    };
+
     return (
         <ParentGuideContainer>
-            <SearchBar placeholder="Search by specific question" />
+            <SearchBar
+                placeholder="Search by specific question"
+                onChangeText={setInput}
+            />
+            <StyledMargin>
+                <StyledList
+                    data={output.slice(0, 5)}
+                    keyExtractor={q => q.Question}
+                    extraData={output}
+                    renderItem={({ item }) => (
+                        <MatchBorder>
+                            <AutoMatch>{`${item.Question}`}</AutoMatch>
+                        </MatchBorder>
+                    )}
+                />
+            </StyledMargin>
             <ViewHeading> View By: </ViewHeading>
             <ViewBy> grouped interpretation </ViewBy>
             <View>
